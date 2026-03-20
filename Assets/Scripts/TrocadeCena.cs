4f5772d8494522cc944afe5ca1e827e8f47afa19
fbsjao748 <borgesfernando450@gmail.com>
2026-03-20 19:18:30 -0300

    codigo do script
    fizemos o script do jogo

---
+++ Assets/Scripts/TrocadeCena.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public string nomeDaCena;
+
+    //Metodo para corrotina Abrir
+    public void Procurar()
+    {
+       StartCoroutine(Abrir());
+    }
+    
+    //Metodo para fechar a aplicação
+    public void Exit()
+    {
+       StartCoroutine(Exits());
+
+       //essa linha fecha o play após a morte do jogador
+       #if UNITY_EDITOR
+           UnityEditor.EditorApplication.isPlaying = false;
+       #endif
+    }
+    
+    //Metodo para abrir a variavel especifica
+    private IEnumerator Abrir()
+    {
+       yield return new WaitForSeconds(1.4f);
+       SceneManager.LoadScene(nomeDaCena);
+    }
+
+    //Metodo para sair com tempo
+    public IEnumerator Exits()
+    {
+       yield return new WaitForSeconds(1.4f);
+       Application.Quit();
+    }
+
+}
